4930c49fcbaad2c351f8748676b895fedf4150a0
RonyOtaku <ronaldoimportacoes@gmail.com>
2024-03-11 08:40:07 -0300

    Cripto

--- script.js
+++ script.js
@@ -1,34 +1,88 @@
-const form = document.getElementById('form');
+async function searchCrypto() {
+    const cryptoName = document.getElementById('searchInput').value;
+    const apiKey = '04f19d41-6e31-4850-88c4-c721cf420aa8';
 
-form.addEventListener('submit', function(event){
-    event.preventDefault();
+    try {
+        const response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?&convert=USD&symbol=${cryptoName}&CMC_PRO_API_KEY=${apiKey});
+        const data = await response.json();
 
-    const weight = parseFloat(document.getElementById('weight').value);
-    const height = parseFloat(document.getElementById('height').value);
+        console.log('Resposta da API:', data);
 
-    const bmi = (weight / (height * height)).toFixed(2);
+        if (data.status.error_code === 0 && data.data && data.data.length > 0) {
+            const cryptoInfo = data.data[0];
 
-    const value = document.getElementById('value');
-    let description = '';
-    
-    value.classList.add('attention');
-    document.getElementById('infos').classList.remove('hidden');
+            // Atualize os valores conforme necessário
+            const precoInput = document.getElementById('precoImput');
+            precoInput.value = cryptoInfo.quote.USD.price.toFixed(2);
 
-    if (bmi < 18.5) {
-        description = 'Cuidado! Você está abaixo do peso!';
-    } else if (bmi >= 18.5 && bmi <= 25) {
-        description = 'Você está no peso ideal!';
-        value.classList.remove('attention');
-        value.classList.add('normal');
-    } else if (bmi > 25 && bmi <= 30) {
-        description = 'Cuidado! Você está com sobrepeso!';
-    } else if (bmi > 30 && bmi <= 35) {
-        description = 'Cuidado! Você está com obesidade moderada!';
-    } else if (bmi > 35 && bmi <= 40) {
-        description = 'Cuidado! Você está com obesidade severa!';
-    } else {
-        description = 'Cuidado! Você está com obesidade mórbida!';
+            const totalCirculanteInput = document.getElementById('totalCirculanteInput');
+            totalCirculanteInput.value = cryptoInfo.total_supply || 'N/A';
+
+            const capMercadoInput = document.getElementById('capMercadoInput');
+            capMercadoInput.value = cryptoInfo.quote.USD.market_cap || 'N/A';
+        } else {
+            console.error('Erro na resposta da API:', data.status.error_message);
+        }
+    } catch (error) {
+        console.error('Erro ao recuperar dados:', error);
     }
-    value.textContent = bmi.replace('.', ',');
-    document.getElementById('description').textContent = description;
+}
+
+function formatNumber(number, maxFractionDigits) {
+    const options = {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: maxFractionDigits,
+    };
+    return new Intl.NumberFormat('en-US', options).format(number);
+}
+
+function parseFormattedNumber(value) {
+    const cleanValue = value.replace(/[^\d.-]/g, '');
+    return parseFloat(cleanValue) || 0;
+}
+
+function formatAndSetInputValue(inputId, number, maxFractionDigits) {
+    const inputValue = document.getElementById(inputId);
+    inputValue.value = formatNumber(number, maxFractionDigits);
+}
+
+function updateFormattedValue(inputId) {
+    const inputValue = document.getElementById(inputId);
+    const parsedValue = parseFormattedNumber(inputValue.value);
+    formatAndSetInputValue(inputId, parsedValue, 30); // 30 é o número máximo de zeros após a vírgula
+}
+
+function calculate() {
+    const percCrescimentoInput = document.getElementById('percCrescimentoInput');
+    const totalCirculanteInput = document.getElementById('totalCirculanteInput');
+
+    const capMercadoInput = parseFormattedNumber(document.getElementById('capMercadoInput').value) || 0;
+    const totalCirculante = parseFormattedNumber(totalCirculanteInput.value) || 1;
+    const percCrescimento = parseFormattedNumber(percCrescimentoInput.value) || 0;
+
+    const novaCapMercado = capMercadoInput + (capMercadoInput * percCrescimento / 100);
+    formatAndSetInputValue('novaCapMercadoInput', novaCapMercado.toFixed(2), 30);
+
+    if (!isNaN(novaCapMercado) && !isNaN(totalCirculante)) {
+        const resultadoCalculo = novaCapMercado / totalCirculante;
+        formatAndSetInputValue('novoPreçoInput', resultadoCalculo, 30);
+    } else {
+        alert('Valores inválidos para o cálculo.');
+    }
+}
+
+['capMercadoInput', 'totalCirculanteInput', 'percCrescimentoInput'].forEach(function(inputId) {
+    document.getElementById(inputId).addEventListener('input', function() {
+        updateFormattedValue(inputId);
+    });
+
+    document.getElementById(inputId).addEventListener('keydown', function(event) {
+        if (event.key === 'Backspace' || event.key === 'Delete') {
+            updateFormattedValue(inputId);
+        }
+    });
+});
+
+document.getElementById('calculate').addEventListener('click', function() {
+    calculate();
 });
